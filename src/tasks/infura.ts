e4b694662c3e9e6bb18f2de8bc3ad84a4f371075
noahehall <noahehall@users.noreply.github.com>
2022-04-21 15:51:33 -0400

    get random acc balance and update tsconfig to permit top level await

--- src/tasks/infura.ts
+++ src/tasks/infura.ts
@@ -4,4 +4,8 @@ const url = 'https://mainnet.infura.io/v3/e2731feb01324697b960dd1494aaf940';
 
 const api = new Web3(url);
 
-console.info('\n\n web3', api);
+const randomAddress = '0x00000000219ab540356cBB839Cbe05303d7705Fa';
+
+const balance = await api.eth.getBalance(randomAddress);
+
+console.info('\n\n account balance', balance);
